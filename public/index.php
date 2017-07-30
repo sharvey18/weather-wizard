53d9dd55edce017b96d78f5a167f8b61553282d9
Sam Harvey <s.rharveyiv@gmail.com>
2017-07-30 12:17:54 -0700

    initial
    initial commit

---
+++ public/index.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Horoscope Teller</title>
+    <link rel="stylesheet" href="sass/app.min.css">
+  </head>
+  <body>
+
+    <!-- <div>
+      <p class="greeting-id">The ID is </p>
+      <p class="greeting-content">The content is </p>
+    </div> -->
+
+    <h1>Weather Wizard</h1>
+    <span id="error"></span>
+
+    <div class="container">
+      <input type="text" name="city" id="city" placeholder="City Name">
+      <button type="button" name="button" id="submitWeather">Search City</button>
+    </div>
+
+    <div id="show">
+
+    </div>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" charset="utf-8"></script>
+    <script src="js/scripts.js" charset="utf-8"></script>
+  </body>
+</html>
